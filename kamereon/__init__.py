edf8e26738f23fc3b4c0172306af86227e586d7b
Richard Mitchell <richard.j.mitchell@gmail.com>
2020-04-02 19:53:53 +0100

    Remove unused gunk

--- kamereon/__init__.py
+++ kamereon/__init__.py
@@ -30,64 +30,16 @@ DATA_KEY = DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 MIN_UPDATE_INTERVAL = timedelta(minutes=1)
-DEFAULT_UPDATE_INTERVAL = timedelta(minutes=1)
+DEFAULT_UPDATE_INTERVAL = timedelta(minutes=5)
 
 CONF_MANUFACTURER = 'manufacturer'
 CONF_REGION = "region"
-CONF_MUTABLE = "mutable"
 
 SIGNAL_STATE_UPDATED = f"{DOMAIN}.updated"
 
 MANUFACTURERS = {
     'nissan': NCISession,
 }
-
-COMPONENTS = {
-    "sensor": "sensor",
-    "binary_sensor": "binary_sensor",
-    "lock": "lock",
-    "device_tracker": "device_tracker",
-    "switch": "switch",
-}
-
-RESOURCES = [
-    "position",
-    "lock",
-    "heater",
-    "odometer",
-    "trip_meter1",
-    "trip_meter2",
-    "fuel_amount",
-    "fuel_amount_level",
-    "average_fuel_consumption",
-    "distance_to_empty",
-    "washer_fluid_level",
-    "brake_fluid",
-    "service_warning_status",
-    "bulb_failures",
-    "battery_range",
-    "battery_level",
-    "time_to_fully_charged",
-    "battery_charge_status",
-    "engine_start",
-    "last_trip",
-    "is_engine_running",
-    "doors_hood_open",
-    "doors_front_left_door_open",
-    "doors_front_right_door_open",
-    "doors_rear_left_door_open",
-    "doors_rear_right_door_open",
-    "windows_front_left_window_open",
-    "windows_front_right_window_open",
-    "windows_rear_left_window_open",
-    "windows_rear_right_window_open",
-    "tyre_pressure_front_left_tyre_pressure",
-    "tyre_pressure_front_right_tyre_pressure",
-    "tyre_pressure_rear_left_tyre_pressure",
-    "tyre_pressure_rear_right_tyre_pressure",
-    "any_door_open",
-    "any_window_open",
-]
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -101,9 +53,6 @@ CONFIG_SCHEMA = vol.Schema(
                 ): vol.All(cv.time_period, vol.Clamp(min=MIN_UPDATE_INTERVAL)),
                 vol.Optional(CONF_NAME, default={}): cv.schema_with_slug_keys(
                     cv.string
-                ),
-                vol.Optional(CONF_RESOURCES): vol.All(
-                    cv.ensure_list, [vol.In(RESOURCES)]
                 ),
                 vol.Optional(CONF_REGION): cv.string,
             }
